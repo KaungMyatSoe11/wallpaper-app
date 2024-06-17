573cf1dcbbbe726c953c5176b21b5b0ce043a1cb
Kaung Myat Soe <52688252+KaungMyatSoe11@users.noreply.github.com>
2024-06-17 20:31:16 +0630

    add category and image list
    Signed-off-by: Kaung Myat Soe <52688252+KaungMyatSoe11@users.noreply.github.com>

--- app/home/index.js
+++ app/home/index.js
@@ -6,18 +6,40 @@ import {
   ScrollView,
   TextInput,
 } from "react-native";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { Feather, FontAwesome6, Ionicons } from "@expo/vector-icons";
 import { theme } from "../../constants/theme";
 import { hp, wp } from "../../helpers/common";
+import Categories from "../../components/categories";
+import { apiCall } from "../../api";
+import ImageGrid from "../../components/imageGrid";
 
 const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
   const paddingTop = top > 0 ? top + 10 : 30;
   const [search, setSearch] = useState("");
+  const [images, setImages] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(null);
   const refSearchInput = useRef(null);
-  console.log(search);
+  console.log(activeCategory);
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  const fetchImages = async (params = { page: 1 }, append = true) => {
+    let res = await apiCall(params);
+    if (res.success && res?.data?.hits) {
+      if (append) {
+        setImages([...images, ...res.data.hits]);
+      } else {
+        setImages([...res.data.hits]);
+      }
+    }
+  };
+  const handleChangeCategory = (value) => {
+    setActiveCategory(value);
+  };
   return (
     <View style={[styles.container, { paddingTop }]}>
       <View style={styles.header}>
@@ -51,6 +73,17 @@ const HomeScreen = () => {
               />
             </Pressable>
           )}
+        </View>
+
+        <Categories
+          activeCategory={activeCategory}
+          handleChangeCategory={handleChangeCategory}
+        />
+
+        <View>
+          {
+            images.length>0 && <ImageGrid images={images}/>
+          }
         </View>
       </ScrollView>
     </View>
